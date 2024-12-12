66d4c00633ef9db1f8ca68aea8bdef0031368bb1
Coding-Chobo <dbdaldrb1@gmail.com>
2024-12-12 20:30:01 +0900

    플레이어 충돌 메커니즘 수정 및 보스 어택 틀

---
+++ coumputer_grapics_endproject/coumputer_grapics_endproject/BossAttack.cpp
@@ -0,0 +1,26 @@
+#include "BossAttack.h"
+#include "Boss.h"
+#include <iostream>
+#include <GL/glew.h>
+#include "CollisionManger.h"
+#include "Object.h"
+BossAttack::BossAttack(Boss* boss)
+{
+	this->boss = boss;
+}
+void BossAttack::closeAttack(){
+
+}
+void BossAttack::Slam() {
+
+}
+void BossAttack::closeAttack() {
+
+}
+void BossAttack::Dash() {
+
+}
+void BossAttack::Wander() {
+
+}
+
--- coumputer_grapics_endproject/coumputer_grapics_endproject/BossAttack.h
+++ coumputer_grapics_endproject/coumputer_grapics_endproject/BossAttack.h
@@ -1,1 +1,38 @@
 #pragma once
+#include "gl/glm/glm.hpp"
+#include <map>
+
+class Model;
+class Collision;
+class Animation;
+class Animator;
+class Terrain;
+class CameraBase;
+class DirectionalLight;
+class PointLight;
+class Object;
+class Player;
+class Boss;
+
+class BossAttack
+{
+public:
+	BossAttack(Boss* boss);
+	int GetStatus() { return status; }
+	Boss* GetBoss() { return boss; }
+	void Slam();
+	void closeAttack();
+	void Dash();
+	void Wander();
+	
+	 
+private:
+	Boss* boss;
+	int status;
+	//bool Slam; //--내려찍는 스킬
+	//bool closeAttack; // 기본 근접 공격
+	////bool closeAttack;
+	//bool Dash; // 플레이어의 위치로 대쉬
+	
+};
+
